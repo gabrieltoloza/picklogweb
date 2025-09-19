a28947bf0d22752ad5af78ffc808d14ff2c324be
gabrieltoloza <estratogerman@gmail.com>
2025-09-19 14:12:18 -0300

    corrigiendo responsividad de Soluciones para monitores grandes

--- app/components/Soluciones/DropdownSolutions.vue
+++ app/components/Soluciones/DropdownSolutions.vue
@@ -4,17 +4,17 @@
 
         <!-- Botón 1 -->
         <div class="dropdown dropdown-hover relative">
-            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] h-[48px] rounded-[36px] py-4 px-4 flex items-center justify-between 
-               bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
-                <span class="font-medium text-sm sm:text-base">TMS</span>
-                <svg width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] h-[48px] md:h-[56px] lg:h-[64px] rounded-[36px] py-4 px-4 md:py-5 md:px-6 flex items-center justify-between 
+        bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
+                <span class="font-medium text-sm sm:text-base md:text-lg lg:text-xl">TMS</span>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 8H16" stroke="#121A1F" stroke-width="2" />
                     <path d="M8 16L8 0" stroke="#121A1F" stroke-width="2" />
                 </svg>
             </div>
             <ul tabindex="0" class="dropdown-button dropdown-content menu absolute left-0 top-full bg-transparent rounded-[14px] z-10 
-               w-[180px] sm:w-[220px] p-2 mt-3 shadow-lg border border-gray-400">
-                <p class="text-white text-sm sm:text-base p-4 sm:p-5">
+        w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] p-2 md:p-4 mt-3 shadow-lg border border-gray-400">
+                <p class="text-white text-sm sm:text-base md:text-lg p-4 sm:p-5 md:p-6">
                     Dashboard para monitorear el estado de los pedidos en tiempo real.
                 </p>
             </ul>
@@ -23,8 +23,8 @@
 
         <!-- Botón 2 -->
         <div class="dropdown dropdown-hover">
-            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] h-[48px] rounded-[36px] py-4 px-4 flex items-center justify-between 
-               bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
+            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] h-[48px] md:h-[56px] lg:h-[64px] rounded-[36px] py-4 px-4 md:py-5 md:px-6 flex items-center justify-between 
+        bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
                 <span class="font-medium text-sm sm:text-base">WMS</span>
                 <svg width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 8H16" stroke="#121A1F" stroke-width="2" />
@@ -32,7 +32,7 @@
                 </svg>
             </div>
             <ul tabindex="0" class="dropdown-button dropdown-content menu absolute left-0 top-full bg-transparent rounded-[14px] z-10 
-               w-[180px] sm:w-[220px] p-2 mt-3 shadow-lg border border-gray-400">
+        w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] p-2 md:p-4 mt-3 shadow-lg border border-gray-400">
                 <p class="text-white text-sm sm:text-base p-4 sm:p-5">
                     Trazabilidad en línea las 24 horas, los 365 días del año.
                     Seguimiento de inventario en tiempo real para reponer a tiempo y evitar quiebres de stock.
@@ -42,8 +42,8 @@
 
         <!-- Botón 3 -->
         <div class="dropdown dropdown-hover">
-            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] h-[48px] rounded-[36px] py-4 px-4 flex items-center justify-between 
-               bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
+            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] h-[48px] md:h-[56px] lg:h-[64px] rounded-[36px] py-4 px-4 md:py-5 md:px-6 flex items-center justify-between 
+        bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
                 <span class="font-medium text-sm sm:text-base">E-COMMERCE</span>
                 <svg width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 8H16" stroke="#121A1F" stroke-width="2" />
@@ -51,7 +51,7 @@
                 </svg>
             </div>
             <ul tabindex="0" class="dropdown-button dropdown-content menu absolute left-0 top-full bg-transparent rounded-[14px] z-10 
-               w-[180px] sm:w-[220px] p-2 mt-3 shadow-lg border border-gray-400">
+        w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] p-2 md:p-4 mt-3 shadow-lg border border-gray-400">
                 <p class="text-white text-sm sm:text-base p-4 sm:p-5">
                     Integración a medida con tu e-commerce y/o marketplace para gestionar tus ventas.
                 </p>
@@ -60,8 +60,8 @@
 
         <!-- Botón 4 -->
         <div class="dropdown dropdown-hover relative group">
-            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] h-[48px] rounded-[36px] py-4 px-4 flex items-center justify-between 
-               bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
+            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] h-[48px] md:h-[56px] lg:h-[64px] rounded-[36px] py-4 px-4 md:py-5 md:px-6 flex items-center justify-between 
+        bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
                 <span class="w-[80%] font-medium text-sm ">TOMADOR DE PEDIDOS WEB</span>
                 <svg width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 8H16" stroke="#121A1F" stroke-width="2" />
@@ -69,7 +69,7 @@
                 </svg>
             </div>
             <ul tabindex="0" class="dropdown-button dropdown-content menu absolute left-0 top-full bg-transparent rounded-[14px] z-10 
-               w-[180px] sm:w-[220px] p-2 mt-3 shadow-lg border border-gray-400">
+        w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] p-2 md:p-4 mt-3 shadow-lg border border-gray-400">
                 <p class="text-white text-sm sm:text-base p-4 sm:p-5">
                     Ingresa pedidos online.
                     Desde cualquier lugar y en cualquier momento.
@@ -79,8 +79,8 @@
 
         <!-- Botón 5 -->
         <div class="dropdown dropdown-hover">
-            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] h-[48px] rounded-[36px] py-4 px-4 flex items-center justify-between 
-               bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
+            <div tabindex="0" role="button" class="w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] h-[48px] md:h-[56px] lg:h-[64px] rounded-[36px] py-4 px-4 md:py-5 md:px-6 flex items-center justify-between 
+        bg-gray-200 text-gray-800 hover:bg-[#F9BD6B] hover:text-gray-800 transition-all duration-300">
                 <span class="font-medium text-sm ">EQUIPO DE IT DISPONIBLE</span>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
@@ -88,7 +88,7 @@
                 </svg>
             </div>
             <ul tabindex="0" class="dropdown-button dropdown-content menu absolute left-0 top-full bg-transparent rounded-[14px] z-10 
-               w-[180px] sm:w-[220px] p-2 mt-3 shadow-lg border border-gray-400">
+        w-[180px] sm:w-[220px] md:w-[260px] lg:w-[300px] p-2 md:p-4 mt-3 shadow-lg border border-gray-400">
                 <p class="text-white text-sm sm:text-base p-4 sm:p-5">
                     Soporte técnico especializado para acompañar tu operación <b>cuando lo necesites</b>.
                 </p>
